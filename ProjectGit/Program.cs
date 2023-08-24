862f062eeb5ba76c7744b8c22824e64ce71cbe59
Terest1an <nktv21info@gmail.com>
2023-08-24 17:43:19 +0300

    first Commit

--- ProjectGit/Program.cs
+++ ProjectGit/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectGit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //sumtask_1 changes
+            //sumtask_2 changes
+        }
+    }
+}
